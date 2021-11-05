258374339074a439c5b0a409665eda9c17964d04
Prince-Christopher <christopherprince35@gmail.com>
2021-11-05 23:35:14 +0530

    Added start activity

--- app/src/main/java/com/example/ethereal/StartActivity.java
+++ app/src/main/java/com/example/ethereal/StartActivity.java
@@ -2,13 +2,28 @@ package com.example.ethereal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+
+    Button getstarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        getstarted = findViewById(R.id.getstarted);
+        getstarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(StartActivity.this, JoinActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
