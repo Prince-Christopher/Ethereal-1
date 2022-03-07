48b17813c2633b47cf4ca3f60e67998f52e50365
Prince-Christopher <christopherprince35@gmail.com>
2022-03-07 07:14:02 +0530

    committed

--- app/src/main/java/Fragments/MoodsFragment.java
+++ app/src/main/java/Fragments/MoodsFragment.java
@@ -1,5 +1,7 @@
 package Fragments;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,20 +11,207 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.ethereal.Activities.InsightsActivity;
 import com.example.ethereal.R;
+import com.google.android.material.card.MaterialCardView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MoodsFragment extends Fragment {
-
+    public int happyCounter = 0, relaxedCounter = 0, neutralCounter = 0, sadCounter = 0, angryCounter = 0, fearCounter = 0, proudCounter = 0, sickCounter = 0, sillyCounter = 0;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    MaterialCardView happycard, relaxedcard, neutralcard, sadcard, angrycard, fearcard, proudcard, sickcard, sillycard;
+    ImageView insights;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_moods, container, false);
-        return view;
 
-    }
-}
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Moods");
+        String hCounter, rCounter, nCounter, sCounter, aCounter, fCounter, pCounter, sicCounter, silCounter;
+
+        hCounter = String.valueOf(happyCounter);
+        databaseReference.child("happycard").setValue(hCounter);
+
+        rCounter = String.valueOf(relaxedCounter);
+        databaseReference.child("relaxedcard").setValue(rCounter);
+
+        nCounter = String.valueOf(neutralCounter);
+        databaseReference.child("neutralcard").setValue(nCounter);
+
+        sCounter = String.valueOf(sadCounter);
+        databaseReference.child("sadcard").setValue(sCounter);
+
+        aCounter = String.valueOf(angryCounter);
+        databaseReference.child("angrycard").setValue(aCounter);
+
+        fCounter = String.valueOf(fearCounter);
+        databaseReference.child("fearcard").setValue(fCounter);
+
+        pCounter = String.valueOf(proudCounter);
+        databaseReference.child("proudcard").setValue(pCounter);
+
+        sicCounter = String.valueOf(sickCounter);
+        databaseReference.child("sickcard").setValue(sicCounter);
+
+        silCounter = String.valueOf(sillyCounter);
+        databaseReference.child("sillycard").setValue(silCounter);
+
+        happycard = view.findViewById(R.id.happycard);
+        relaxedcard = view.findViewById(R.id.relaxedcard);
+        neutralcard = view.findViewById(R.id.neutralcard);
+        sadcard = view.findViewById(R.id.sadcard);
+        angrycard = view.findViewById(R.id.angrycard);
+        fearcard = view.findViewById(R.id.fearcard);
+        proudcard = view.findViewById(R.id.proudcard);
+        sickcard = view.findViewById(R.id.sickcard);
+        sillycard = view.findViewById(R.id.sillycard);
+
+        insights = view.findViewById(R.id.insights);
+        insights.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(), InsightsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        happycard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(getActivity());
+                dialog.setContentView(R.layout.moodsdialog);
+                dialog.getWindow().setBackgroundDrawableResource(R.color.happyyellow);
+                dialog.show();
+                happyCounter++;
+                String hCounter;
+                hCounter = String.valueOf(happyCounter);
+                databaseReference.child("happycard").setValue(hCounter);
+            }
+    });
+
+        relaxedcard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(getActivity());
+                dialog.setContentView(R.layout.moodsdialog);
+                dialog.getWindow().setBackgroundDrawableResource(R.color.relaxedcerulean);
+                dialog.show();
+                relaxedCounter++;
+                String rCounter;
+                rCounter = String.valueOf(relaxedCounter);
+                databaseReference.child("relaxedcard").setValue(rCounter);
+            }
+        });
+
+        neutralcard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(getActivity());
+                dialog.setContentView(R.layout.moodsdialog);
+                dialog.getWindow().setBackgroundDrawableResource(R.color.neutralpink);
+                dialog.show();
+                neutralCounter++;
+                String nCounter;
+                nCounter = String.valueOf(neutralCounter);
+                databaseReference.child("neutralcard").setValue(nCounter);
+            }
+        });
+
+        sadcard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(getActivity());
+                dialog.setContentView(R.layout.moodsdialog);
+                dialog.getWindow().setBackgroundDrawableResource(R.color.sadpurple);
+                dialog.show();
+                sadCounter++;
+                String saCounter;
+                saCounter = String.valueOf(sadCounter);
+                databaseReference.child("sadcard").setValue(saCounter);
+            }
+        });
+
+        angrycard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(getActivity());
+                dialog.setContentView(R.layout.moodsdialog);
+                dialog.getWindow().setBackgroundDrawableResource(R.color.angryred);
+                dialog.show();
+                angryCounter++;
+                String aCounter;
+                aCounter = String.valueOf(angryCounter);
+                databaseReference.child("angrycard").setValue(aCounter);
+            }
+        });
+
+        fearcard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(getActivity());
+                dialog.setContentView(R.layout.moodsdialog);
+                dialog.getWindow().setBackgroundDrawableResource(R.color.fearycyan);
+                dialog.show();
+                fearCounter++;
+                String fCounter;
+                fCounter = String.valueOf(fearCounter);
+                databaseReference.child("fearcard").setValue(fCounter);
+            }
+        });
+
+        proudcard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(getActivity());
+                dialog.setContentView(R.layout.moodsdialog);
+                dialog.getWindow().setBackgroundDrawableResource(R.color.proudtangerine);
+                dialog.show();
+                proudCounter++;
+                String pCounter;
+                pCounter = String.valueOf(proudCounter);
+                databaseReference.child("proudcard").setValue(pCounter);
+            }
+        });
+
+        sickcard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(getActivity());
+                dialog.setContentView(R.layout.moodsdialog);
+                dialog.getWindow().setBackgroundDrawableResource(R.color.sickviolet);
+                dialog.show();
+                sickCounter++;
+                String sicCounter;
+                sicCounter = String.valueOf(sickCounter);
+                databaseReference.child("sickcard").setValue(sicCounter);
+            }
+        });
+
+        sillycard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(getActivity());
+                dialog.setContentView(R.layout.moodsdialog);
+                dialog.getWindow().setBackgroundDrawableResource(R.color.sillyred);
+                dialog.show();
+                sillyCounter++;
+                String silCounter;
+                silCounter = String.valueOf(sillyCounter);
+                databaseReference.child("sillycard").setValue(silCounter);
+            }
+        });
+
+    return view;
+
+}}
---
+++ app/src/main/java/com/example/ethereal/Activities/InsightsActivity.java
@@ -0,0 +1,183 @@
+package com.example.ethereal.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.ethereal.R;
+import com.google.android.material.card.MaterialCardView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import Fragments.MoodsFragment;
+
+public class InsightsActivity extends AppCompatActivity {
+
+    MaterialCardView insightsback;
+    TextView happycount, relaxedcount, neutralcount, sadcount, angrycount, fearcount, proudcount, sickcount, sillycount;
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insights);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                window.setStatusBarColor(getColor(R.color.white));
+                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            }
+        }
+        insightsback = findViewById(R.id.insightsback);
+        insightsback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(InsightsActivity.this, ProfileActivity.class);
+                startActivity(i);
+            }
+        });
+        firebaseDatabase =  FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Moods");
+
+        happycount = findViewById(R.id.happycount);
+        databaseReference.child("happycard").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String count = snapshot.getValue(String.class);
+                happycount.setText(count);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        relaxedcount = findViewById(R.id.relaxedcount);
+        databaseReference.child("relaxedcard").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String count = snapshot.getValue(String.class);
+                relaxedcount.setText(count);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        neutralcount = findViewById(R.id.neutralcount);
+        databaseReference.child("neutralcard").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String count = snapshot.getValue(String.class);
+                neutralcount.setText(count);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        sadcount = findViewById(R.id.sadcount);
+        databaseReference.child("sadcard").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String count = snapshot.getValue(String.class);
+                sadcount.setText(count);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        angrycount = findViewById(R.id.angrycount);
+        databaseReference.child("angrycard").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String count = snapshot.getValue(String.class);
+                angrycount.setText(count);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        fearcount = findViewById(R.id.fearcount);
+        databaseReference.child("fearcard").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String count = snapshot.getValue(String.class);
+                fearcount.setText(count);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        proudcount = findViewById(R.id.proudcount);
+        databaseReference.child("proudcard").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String count = snapshot.getValue(String.class);
+                proudcount.setText(count);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        sickcount = findViewById(R.id.sickcount);
+        databaseReference.child("sickcard").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String count = snapshot.getValue(String.class);
+                sickcount.setText(count);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        sillycount = findViewById(R.id.sillycount);
+        databaseReference.child("sillycard").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String count = snapshot.getValue(String.class);
+                sillycount.setText(count);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+    }
+}
